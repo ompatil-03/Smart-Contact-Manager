a4d6e664e7627587727ed712e5475ae9b6fb7827
Mech-Boy <om294729@gmail.com>
2024-06-19 16:59:12 +0530

    Understanding and managing Fragments

--- src/main/java/com/scm/scm10/Controller/PageController.java
+++ src/main/java/com/scm/scm10/Controller/PageController.java
@@ -3,6 +3,9 @@ package com.scm.scm10.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -16,6 +19,19 @@ public class PageController {
         model.addAttribute("city", "Mumbai");
         System.out.println("Home page handeler");
         return "Home";
+    }
+    
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("loggedin",true);
+        System.out.println("About page handeler");
+        return "About";
+    }
+    @RequestMapping("/services")
+    public String requestMethodName(Model model) {
+        model.addAttribute("loggedin",true);
+        System.out.println("Services page handeler");
+        return "services";
     }
     
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["src/main/resources/templates/{.html,.js}"],
+  content: ["./src/main/resources/templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
